30481552c9631bc4ab17c89007dd9ca9b1e60a74
Simon Cruanes <simon.cruanes.2007@m4x.org>
2023-06-05 16:06:22 -0400

    add C++ version of minisat

---
+++ src/minisat-cpp/core/depend.mk
@@ -0,0 +1,27 @@
+/home/simon/w/ocaml-minisat/src/minisat-cpp/core/Main.o /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Main.or /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Main.od /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Main.op: /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Main.cc \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/utils/System.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/IntTypes.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/utils/ParseUtils.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/utils/Options.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Vec.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/XAlloc.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Dimacs.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/core/SolverTypes.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Alg.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Map.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Alloc.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Heap.h
+/home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.o /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.or /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.od /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.op: /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.cc \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Sort.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Vec.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/IntTypes.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/XAlloc.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/core/Solver.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Heap.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Alg.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/utils/Options.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/utils/ParseUtils.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/core/SolverTypes.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Map.h \
+ /home/simon/w/ocaml-minisat/src/minisat-cpp/mtl/Alloc.h
